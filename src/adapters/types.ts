e190b1e0c818b4a754c36b662cb6f291f62f3812
charliesmir <charlie.smirnova@gmail.com>
2026-08-31 11:54:54 +0300

    Sanity connected

---
+++ src/adapters/types.ts
@@ -0,0 +1,55 @@
+export interface SanityMain {
+  titleSection: {
+    name: string;
+    surname: string;
+    about: string;
+    picture: string;
+  };
+  artworksSection: {
+    title: { title: string };
+    gallery: { collections: { artwork: string; name: string }[] };
+  };
+  cvSection: {
+    title: { title: string };
+    buttons: { icon?: string; title?: string };
+    info: {
+      statement?: string;
+      exhibitions?: {
+        exhibitions?: {
+          name: string;
+          place: string;
+          year: string;
+          link?: string;
+        }[];
+      };
+      education?: {
+        educations?: { name: string; place: string; year: string }[];
+      };
+      experience?: {
+        experiences?: { name: string; place: string; year: string }[];
+      };
+    };
+  };
+  contactsSection: {
+    title: { title: string };
+    contacts: {
+      contacts?: { icon: string; name: string; link: string }[];
+    };
+  };
+}
+
+export interface SanityCollection {
+  title: {
+    title: string;
+    idea: string;
+  };
+
+  gallery: {
+    artworks: {
+      image: string;
+      title: string;
+      info: string;
+      description?: string;
+    }[];
+  };
+}
