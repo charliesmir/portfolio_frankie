f9c3a29eae567b2a4183573aa0cdb71062114104
charliesmir <charlie.smirnova@gmail.com>
2026-09-01 18:29:39 +0300

    Design changes and adaptability fixes

--- src/app/homeContent.tsx
+++ src/app/homeContent.tsx
@@ -42,7 +42,6 @@ export default function HomeContent({ sanityImport }: HomeContentProps) {
         onClick: () => scrollToSection(mainRef),
         alignment: "center",
         size: "medium",
-        isActive: true,
       },
       {
         title: "Artworks",
